50277249fd4393df1fcb8c82dca0090bcbedd346
Dave Tapley <dave@tapley.com>
2023-07-27 17:13:19 -0700

    runner

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 from multiprocessing import freeze_support
+from threading import Thread
+from time import sleep
 import click
 
+
+class Runner(object):
+
+    def __init__(self):
+        self.running = False
+
+    def run(self):
+        self.running = True
+        while self.running:
+            print("Running")
+            sleep(1)
+
+    def stop(self):
+        self.running = False
+
+
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo(f"Hello {name}!")
+def hello():
+
+    def _test():
+        click.secho("Waiting", fg="blue")
+        sleep(5)
+        click.secho("Stopping", fg="blue")
+        runner.stop()
+
+    t = Thread(target=_test)
+    t.start()
+
+    click.secho("Starting runner", fg="blue")
+    runner = Runner()
+    runner.run()
+
 
 if __name__ == '__main__':
     freeze_support()
